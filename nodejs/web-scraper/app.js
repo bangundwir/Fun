125219f9268a8fc5b7a6a4833eaf611b49894d48
bangundwir <hendrabangundwir@gmail.com>
2021-10-14 10:51:14 -0700

    scraaper

---
+++ nodejs/web-scraper/app.js
@@ -0,0 +1,27 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+const pretty =require("pretty")
+
+const markup = `
+<ul class="fruits">
+  <li class="fruits__mango"> Mango </li>
+  <li class="fruits__apple"> Apple </li>
+</ul>
+`
+
+const $ = cheerio.load(markup)
+console.log(pretty($.html()))
+
+
+const mango = $(".fruits__mango")
+console.log(mango.html())
+
+const apple = $(".fruits__apple")
+console.log(apple.attr("class"))
+
+
+const listItems = $("li")
+console.log(listItems.length);
+listItems.each(function (idx, el) {
+  console.log($(el).text())
+})
