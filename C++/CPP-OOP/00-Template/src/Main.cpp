d2a3c1f9db5b8ff4da38bd98de3193cd60147067
bangundwir <hendrabangundwir@gmail.com>
2021-11-02 18:26:54 +0700

    C

---
+++ C++/CPP-OOP/00-Template/src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout << "Hello World" << endl;
+    cout << "Arema Singo Edan" << endl;
+    return 0;
+    
+} // namespace name
